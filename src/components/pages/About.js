2926fe9b0982c36edb4869e629d7eb547e76b96b
Chigozie francis <81627242+francisChigozie@users.noreply.github.com>
2024-03-28 15:43:02 +0100

    First commit

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,9 @@ const About = () => {
     <div>
       <h1>About This App</h1>
       <p className='my-1'>
-        Pickk Shop is a shopping Hall where peolpe can find all they could buy and make good contacts with buyers and sellers directly.
+        Pickk Shop is a shopping Hall where peolpe can find all they could buy and make 
+        good contacts with buyers and sellers directly.Help us promote this project.
+        
       </p>
       <p className='bg-dark p'>
         <strong>Version: </strong> 1.0.0
