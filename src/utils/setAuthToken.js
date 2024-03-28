4615692bbf919b02351ee5e8abb9a408053a1492
Chigozie francis <81627242+francisChigozie@users.noreply.github.com>
2024-03-28 13:43:51 +0100

    new project

--- src/utils/setAuthToken.js
+++ src/utils/setAuthToken.js
@@ -4,10 +4,7 @@ const setAuthToken = token => {
   if (token) {
     axios.defaults.headers.common['x-auth-token'] = token;
     localStorage.setItem('token', token);
-  } else {
-    delete axios.defaults.headers.common['x-auth-token'];
-    localStorage.removeItem('token');
-  }
+  } 
 };
 
 export default setAuthToken;
